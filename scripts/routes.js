7de8bf6353780a20c8e75b99ec849db86273521e
Mahmoud Magdy <engmahmoud2016@gmail.com>
2016-08-18 22:00:17 +0200

    Create routes.js

---
+++ scripts/routes.js
@@ -0,0 +1,38 @@
+
+var app = angular.module('Github users', ['ngRoute']);
+
+app.config(function($routeProvider){
+
+	$routeProvider
+        .when('/', {
+                templateUrl : 'views/home.html',
+                controller  : 'homeController',
+                title: 'Home'
+                
+        })
+        .when('/users', {
+             templateUrl : 'views/users.html',
+             controller  : 'usersController',
+             title: 'Users'
+                
+        })
+        .when('/about', {
+             templateUrl : 'views/about.html',
+             controller  : 'aboutController',
+             title: 'About'
+                
+        })
+        .otherwise({
+        		redirectTo: 'views/home.html'
+        });
+
+}).run(['$rootScope', '$location','$route', function($rootScope, $location,$route){
+   var path = function() { return $location.path();};
+   $rootScope.$watch(path, function(newVal, oldVal){
+     $rootScope.activetab = newVal;
+   });
+   $rootScope.$on("$routeChangeSuccess", function(currentRoute, previousRoute){
+    //Change page title, based on Route information
+    $rootScope.title = $route.current.title;
+  });
+}]);
